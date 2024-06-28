9c62bb780554b9740c62e7ee3cdcb8cebea6da2e
Ieejueun <zziya2006@gmail.com>
2024-06-28 21:03:25 +0900

    [Design] 로그인 버튼 위치 변경

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav className="flex mr-8">
-      <div className="">login</div>
+    <nav className="flex mr-4">
+      <Link to="/Login">login</Link>
     </nav>
   );
 };

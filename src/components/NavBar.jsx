072859a920e3c6375a029667edfe29cbac0bd1a2
Ieejueun <zziya2006@gmail.com>
2024-06-28 21:33:39 +0900

    [Design] 텍스트 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <nav className="flex mr-4">
-      <Link to="/Login">login</Link>
+      <Link to="/Login">Login</Link>
     </nav>
   );
 };
